033d3e9dafeae0e2edf4876262cf947455faebcc
bigtendons8 <154099045+bigtendons8@users.noreply.github.com>
2024-08-07 17:34:33 +1000

    fix nesting of double quotes

--- qreate.py
+++ qreate.py
@@ -3,7 +3,7 @@ import argparse
 import os
 
 def main():
-    DEFAULT_DIRECTORY = "/Users/james/Documents/Home/codering/python/"
+    DEFAULT_DIRECTORY = "~/"
 
     parser = argparse.ArgumentParser(description="create python project")
     parser.add_argument("name", type=str, nargs=1)
@@ -18,7 +18,7 @@ def main():
     print(f"Created directory \"{args.name[0]}\"")
     os.chdir(created_directory)
     os.system("touch main.py")
-    os.system("echo \"def main():\n\nif __name__ == \"__main__\":\n    main()\" > main.py")
+    os.system('echo "def main():\n\nif __name__ == \\"__main__\\":\n    main()" > main.py')
     if args.git is True:
         os.system("git init")
         os.system("git add main.py")
